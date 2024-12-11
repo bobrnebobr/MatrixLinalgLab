8e4749deb8509f3d1fa3bfe98b1109e11cc82fba
bobrnebobr <mishabobrov291@gmail.com>
2024-12-11 22:10:43 +0300

    Initial commit

---
+++ matrix.py
@@ -0,0 +1,18 @@
+import typing as tp
+
+
+class Matrix:
+    def __init__(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def determinant(self):
+        pass
