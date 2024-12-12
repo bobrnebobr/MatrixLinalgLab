834c5b84c34f453fdb9824ed37dd06c41ab11ff2
bobrnebobr <mishabobrov291@gmail.com>
2024-12-12 23:30:34 +0300

    add tests pattern

---
+++ matrix/tests/matrix.py
@@ -0,0 +1,29 @@
+import unittest
+
+from matrix import Matrix
+
+
+class TestMatrix(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_subtract(self):
+        pass
+
+    def test_multiply(self):
+        pass
+
+    def test_trace(self):
+        pass
+
+    def test_negative(self):
+        pass
+
+    def test_getitem(self):
+        pass
+
+    def test_setitem(self):
+        pass
