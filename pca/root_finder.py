af8d432056d076623e84227083edbcca0bd859b6
Bobrov Michael <mishabobrov291@gmail.com>
2025-04-23 19:42:06 +0300

    УДАЛЕН ЛИШНИЙ ФУНКЦИОНАЛ

--- pca/root_finder.py
+++ pca/root_finder.py
@@ -1,6 +1,3 @@
-import numpy as np
-import sympy as sp
-
 class RootFinder:
     def __init__(self, function):
         self.function = function
@@ -61,11 +58,6 @@ class RootFinder:
 
         return roots, total_iterations
 
-# Функция для преобразования строкового ввода в callable функцию
-def create_function(func_str):
-    x = sp.symbols('x')
-    func_expr = sp.sympify(func_str)
-    return sp.lambdify(x, func_expr)
 
 # Ввод функции и отрезка пользователем
 # func_input = input("Введите функцию от x (например, x**3 - 6*x**2 + 11*x - 6): ")
---
+++ testing_big_bro.py
@@ -0,0 +1,6 @@
+from matrix import *
+from pca import *
+from pca.find_eigenvalues import find_eigenvalues
+
+matrix = input_matrix()
+find_eigenvalues(matrix)
